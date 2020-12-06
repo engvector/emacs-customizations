a7b5001bc3f09b49406b0e1422d44356ccfa1a31
Vinay Venkatesh <vinayind@gmail.com>
2020-12-06 17:42:52 -0500

    C#/.NET customizations

---
+++ customize/csharp.el
@@ -0,0 +1,6 @@
+(defun my-csharp-mode-hook ()
+  ;; enable the stuff you want for C# here
+  (electric-pair-mode 1)       ;; Emacs 24
+  (electric-pair-local-mode 1) ;; Emacs 25
+  )
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
