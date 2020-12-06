abb9da37c7ecd1874846cf66384d04f6c4273801
Vinay Venkatesh <vinayind@gmail.com>
2020-12-06 15:03:30 -0500

    Added some global settings

--- dotemacs.el
+++ dotemacs.el
@@ -1,4 +1,9 @@
 ;; This file loads specific emacs configuration/customization files.
 
 ;; Use HTTPS for package installation
-(load-file "~/.emacs.d/secure-pkg-source.el")
+(load-file "./.emacs.d/secure-pkg-source.el")
+;; Configure global settings
+(load-file "./.emacs.d/global-settings.el")
+
+;; Remaining configuration requires the 'use-package' package.
+(package-install 'use-package)
