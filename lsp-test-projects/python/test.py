f15b4843a1942487a7470e2a80a09541172054b9
Vinay Venkatesh <vinayind@gmail.com>
2020-12-13 00:52:26 -0500

    Added more lines to test stepwise debugging.

--- lsp-test-projects/python/test.py
+++ lsp-test-projects/python/test.py
@@ -1,7 +1,9 @@
 import sys
 
 def test_func():
-    var str = sys.stdin.readline()
     print ("Test function")
+    print ("Another line of text")
+    print ("I think its too much text... TL:DR")
+    print ("Ok... thats it... I'm quiting!!!")
 
 test_func()
