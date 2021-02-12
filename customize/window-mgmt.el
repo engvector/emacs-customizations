0370ee86cc0869d5fd620ee135200a13b612024c
vinayind <vinayind@hotmail.com>
2021-02-12 11:13:55 -0500

    Updated sticky-buffer minor mode.
    Updated sticky-buffer mode to force window size to be static.
    Also updated file to meet emacs' coding standards.

--- customize/window-mgmt.el
+++ customize/window-mgmt.el
@@ -1,9 +1,24 @@
+;; package --- Customizations for window management.
+
+;;; Commentary:
+;; Custom minor mode and other changes to suit my window management needs.
+
+;;; Code:
+
 ;; Record all window layout changes
 ;; "C-c <right>" and "C-c <left>" lets you move between these changes.
 (when (fboundp 'winner-mode)
   (winner-mode 1))
 
-;; Helper functio - lock window to a specific buffer.
-(define-minor-mode sticky-buffer-mode "Make the current window always display
-    this buffer."  nil " sticky" nil (set-window-dedicated-p (selected-window)
-							     sticky-buffer-mode))
+;; Helper function - lock window to a specific buffer.
+(define-minor-mode sticky-buffer-mode
+  "Make the current window always display this buffer."
+  nil
+  " sticky"
+  nil
+  (progn
+    (set-window-dedicated-p (selected-window) sticky-buffer-mode)
+    (setq-default window-size-fixed t)))
+
+;;; window-mgmt.el ends here
+
